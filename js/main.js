7f4e624721b42ff40f4643903126468416c9d2a5
v-nvtsk <v@nvtsk.ru>
2024-08-03 23:59:52 +0300

    feat(app): add entry-point

--- js/main.js
+++ js/main.js
@@ -1,24 +1,75 @@
-import '../css/style.css'
-import javascriptLogo from '../javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "../css/style.css";
+import { addRippleEffects } from "./animation";
+import { initContacts } from "./components/contacts";
+import { educationCard } from "./components/education-card";
+import { experienceCard } from "./components/experience-card";
+import { interestCard } from "./components/interest-card";
+import { languageItem } from "./components/language-item";
+import { initPersonal } from "./components/personal/personal";
+import { listFill } from "./markup/fill-list";
+import { fetchJSON } from "./service/fetch-json";
+import { saveToPdf } from "./service/save-to-pdf";
+import { clearStorage, readStorage, saveStorage, updateItem } from "./service/storage";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+async function main() {
+  const saveBtn = document.querySelector(".save-btn");
+  const clearBtn = document.querySelector(".clear-btn");
 
-setupCounter(document.querySelector('#counter'))
+  saveBtn.addEventListener("click", saveToPdf);
+  clearBtn.addEventListener("click", clearStorage);
+
+  const data = readStorage() || (await fetchJSON());
+  saveStorage(data);
+
+  const photo = document.querySelector(".header__image");
+  const languageList = document.querySelector(".language__list");
+  const experienceList = document.querySelector(".experience__list");
+  const educationList = document.querySelector(".education__list");
+  const interestsList = document.querySelector(".interests__tags");
+
+  listFill(languageList, data.languages, languageItem);
+  listFill(experienceList, data.experience, experienceCard);
+  listFill(educationList, data.education, educationCard);
+  listFill(interestsList, data.interests, interestCard);
+
+  initPersonal(data.personal.data);
+  initContacts(data.contacts);
+
+  if (photo.src !== data.personal.data.photoURL) {
+    photo.src = data.personal.data.photoURL;
+  }
+  photo.removeAttribute("width");
+  photo.removeAttribute("height");
+
+  addEditableText(data);
+
+  const elements = document.querySelectorAll(".rippled");
+  addRippleEffects(elements);
+}
+
+document.addEventListener("DOMContentLoaded", main);
+
+function addEditableText() {
+  const textItems = document.querySelectorAll("[data-text-item]");
+  const toggleItems = document.querySelectorAll("[data-toggle]");
+  addRippleEffects(textItems);
+
+  textItems.forEach((item) => {
+    item.addEventListener("blur", (event) => {
+      updateItem(event.currentTarget.dataset.textItem, event.currentTarget.textContent);
+    });
+  });
+
+  toggleItems.forEach((item) => {
+    item.addEventListener("click", (event) => {
+      toggleItems.forEach((item) => {
+        if (item.classList.contains(event.currentTarget.className)) {
+          item !== event.currentTarget && item.classList.remove("active");
+          updateItem(item.dataset.toggle, false);
+        }
+      });
+      event.currentTarget.classList.toggle("active");
+      updateItem(event.currentTarget.dataset.toggle, event.currentTarget.classList.contains("active"));
+    });
+  });
+}
