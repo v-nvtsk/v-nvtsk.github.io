5c7c672dcc1265e4ffd5f4bb73a97cee4dc7d950
v-nvtsk <v@nvtsk.ru>
2024-09-29 02:50:00 +0300

    feat(app): update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,46 @@
+import { About } from './components/about'
+import { Contacts } from './components/contacts'
+import { Education } from './components/education'
+import { Experience } from './components/experience'
+import { Languages } from './components/languages'
+import { Person } from './components/person/person-name'
+import { Photo } from './components/photo'
+import { PetProjects } from './components/projects'
+import { Skills } from './components/skills'
 import './index.css'
-import Main from './layout/main'
-import Sidebar from './layout/sidebar'
+import { Main, Sidebar } from './layout'
 import styles from './style.module.css'
+import { fetchJSON } from './service/fetch-json'
+import { isVoid } from './service/is-void'
+import { useEffect, useState } from 'react'
+import { CVData } from './types'
 
 function App() {
+  const [data, setData] = useState<CVData | null>(null)
+
+  useEffect(() => {
+    fetchJSON().then(setData).catch(console.error)
+  }, [])
+
+  if (isVoid(data)) return <p>Loading...</p>
+
+  const { personal, contacts, skills, languages, education } = data
+
   return (
     <div className={styles.page}>
-      <Sidebar />
-      <Main />
+      <Sidebar>
+        <Photo />
+        <Contacts contacts={contacts} />
+        <Skills skills={skills} />
+        <Languages languages={languages} />
+        <Education education={education} />
+      </Sidebar>
+      <Main>
+        <Person name={personal.name} post={personal.post} />
+        <About />
+        <PetProjects />
+        <Experience />
+      </Main>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ErrorBoundary } from './components/error-boundary/index.tsx'
 
 const root = document.getElementById('root')
 
 if (!root) throw new Error('Root element not found')
 
 createRoot(root).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <ErrorBoundary>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </ErrorBoundary>,
 )
