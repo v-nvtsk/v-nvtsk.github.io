f080341a59dedafe219f06741f5b1292bef00555
v-nvtsk <v@nvtsk.ru>
2024-09-29 02:49:21 +0300

    feat(types): add interfaces

---
+++ src/types/index.ts
@@ -0,0 +1,75 @@
+export interface PersonalInfo {
+  name: string
+  post: string
+}
+
+export interface Contacts {
+  phone: string
+  email: string
+  github: string
+  telegram: string
+  city: string
+}
+
+export type ExperienceList = ExperienceItem[]
+
+export interface ExperienceItem {
+  mostRecent?: boolean
+  startDate: string
+  endDate: string
+  companyName: string
+  postTitle: string
+  postDescription: string
+  responsibilities: string[]
+  achievements?: string[]
+}
+
+export interface Skills {
+  title: string
+  list: string[]
+}
+
+export type SkillsList = Skills[]
+
+export interface EducationItem {
+  institution: string
+  specialization: string
+  startDate?: string
+  endDate?: string
+  description?: string
+}
+
+export interface HigherEducationItem extends Omit<EducationItem, 'title'> {
+  faculty?: string
+}
+
+export interface Education {
+  courses: EducationItem[]
+  higherEducation: HigherEducationItem[]
+}
+
+export interface PetProject {
+  title: string
+  stack: string[]
+  link: string
+  description: string
+  hasMobileVersion?: boolean
+}
+
+export type PetProjects = PetProject[]
+
+export interface Language {
+  name: string
+  grade: string
+  mark?: number
+}
+
+export interface CVData {
+  personal: PersonalInfo
+  languages: Language[]
+  contacts: Contacts
+  skills: SkillsList
+  education: Education
+  experience: ExperienceList
+  petProjects: PetProjects
+}
