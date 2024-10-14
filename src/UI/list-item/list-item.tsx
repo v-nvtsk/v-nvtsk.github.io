6e9fc70a6ae795295166d455e8748a725d9f255a
v-nvtsk <v@nvtsk.ru>
2024-10-15 00:57:57 +0300

    feat(UI): add event-driven components

--- src/UI/list-item/list-item.tsx
+++ src/UI/list-item/list-item.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren } from 'react'
+import { PropsWithChildren, useState } from 'react'
 import styles from './style.module.css'
 
 interface ListItemProps extends PropsWithChildren {
@@ -7,11 +7,27 @@ interface ListItemProps extends PropsWithChildren {
 }
 
 export function ListItem({ className, children, hideMark }: ListItemProps) {
+  const [hover, setHover] = useState('')
+
   const markStyle = hideMark ? '' : styles.markedItem
 
+  const hoverHandler = (ev: React.MouseEvent) => {
+    ev.stopPropagation()
+    setHover(styles.hovered)
+  }
+
   return (
-    <li className={[markStyle, className].join(' ')}>
-      {children}
-    </li>
+    <>
+      <li
+        className={[markStyle, className, styles.listItem, hover].join(' ')}
+        onMouseOut={() => setHover('')}
+        onMouseOver={hoverHandler}
+      >
+        <div className={styles.removeBtnWrapper}>
+          <button className={styles.removeBtn}>✕</button>
+        </div>
+        {children}
+      </li>
+    </>
   )
 }
--- src/UI/list/list.tsx
+++ src/UI/list/list.tsx
@@ -1,20 +1,40 @@
-import { PropsWithChildren } from 'react'
+import { PropsWithChildren, useState } from 'react'
 import styles from './style.module.css'
 
 interface ListProps extends PropsWithChildren {
   listStyle?: string
   padding?: number
+  editable?: boolean
 }
 
-export function List({ children, listStyle, padding = 0 }: ListProps) {
+export function List({ children, listStyle, padding = 0, editable = true }: ListProps) {
+  const [hover, setHover] = useState('')
   const style: Record<string, string> | null = {
     listStyleType: listStyle ?? 'none',
     paddingLeft: padding + 'px',
   }
 
+  const hoverHandler = (ev: React.MouseEvent) => {
+    ev.stopPropagation()
+    setHover(styles.hovered)
+  }
+
   return (
-    <ul className={styles.list} style={style}>
-      {children}
-    </ul>
+    <div>
+      <ul
+        className={[styles.list, hover].join(' ')}
+        onMouseOut={() => setHover('')}
+        onMouseMove={hoverHandler}
+        style={style}
+      >
+        {children}
+        {editable
+        && (
+          <div className={styles.appendButtons}>
+            <button className={styles.appendBtn}>+</button>
+          </div>
+        )}
+      </ul>
+    </div>
   )
 }
