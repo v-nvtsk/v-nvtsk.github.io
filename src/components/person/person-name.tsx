6a93a09c1f6be38d7fbce979ba5e64f55709ec9f
v-nvtsk <v@nvtsk.ru>
2025-05-04 20:10:02 +0300

    feat: enhance About, Experience, Person, and Projects components with improved styling and layout

--- src/components/person/person-name.tsx
+++ src/components/person/person-name.tsx
@@ -10,14 +10,16 @@ export function Person({ name, post, showQr = false }: Props) {
   const vCard = 'https://v-nvtsk.github.io'
 
   return (
-    <div className={styles.personBlock}>
-      <div className={styles.info}>
-        <h1 className="person__name">{name}</h1>
-        <h2 className="person__position">{post}</h2>
+    <section className={styles.personBlock}>
+      <div className={styles.personWrapper}>
+        <div className={styles.info}>
+          <h1 className={styles.personName}>{name}</h1>
+          <h2 className={styles.personPosition}>{post}</h2>
+        </div>
+        <div className={showQr ? styles.qrPdf : styles.qr}>
+          <QR text={vCard} />
+        </div>
       </div>
-      <div className={showQr ? styles.qrPdf : styles.qr}>
-        <QR text={vCard} />
-      </div>
-    </div>
+    </section>
   )
 }
